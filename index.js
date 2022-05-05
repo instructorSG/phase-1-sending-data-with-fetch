5cf4b97890226e03e6c7e4cfa1f87c67e8496c36
Sara Ginsburg <sara.ginsburg@flatironschool.com>
2022-05-05 16:01:22 -0400

    commit tree db2a72979e7f6d61c1ffea3d51765268958e5f18

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+const submitData = (nm, eml) => {
+  const configurationObject = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify ({name: nm, email: eml}),
+  };
+
+  return fetch ('http://localhost:3000/users', configurationObject)
+    .then (function (response) {
+      return response.json ();
+    })
+    .then (function (object) {
+      document.body.innerHTML = object.id;
+    })
+    .catch (function (error) {
+      document.body.innerHTML = 'Unauthorized Access';
+    });
+};
+submitData ('sara', 'sara@example.com');
